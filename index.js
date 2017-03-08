e5deb0152177bbaa46556d97f5f21ce4acdf3542
Maxime Louet <saumon@maximelouet.me>
2017-03-08 21:33:51 +0100

    Revert to port 80

--- index.js
+++ index.js
@@ -115,6 +115,6 @@ app.use(function (req, res, next) {
   res.status(404).send('404')
 });
 
-app.listen(3000, function () {
+app.listen(80, function () {
   console.log('Started BLIH Web on port 80.')
 })
