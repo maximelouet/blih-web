1d2f3a9ae4068a174a0dd9ef722d37d37c0a07d7
Maxime Louet <maxime@saumon.io>
2018-03-09 12:44:59 +0100

    Show friendly message on empty repository open and add easter egg

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.disable('x-powered-by')
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
-var VERSION = '1.7.1'
+var VERSION = '1.7.2'
 var SERVER_PORT = 1337
 
 
